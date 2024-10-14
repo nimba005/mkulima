32bdb7c61872992149a915efaf6c1c09ed2595df
nimba005 <nimbaoyugi@gmail.com>
2024-10-14 23:20:41 +0300

    Added websocketserver components

---
+++ server/webSocketServer/webSocketServer.js
@@ -0,0 +1,33 @@
+/*const http = require("http");
+const express = require("express");
+// const socketIo = require("socket.io");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+// const server = http.createServer(app);
+// const io = socketIo(server);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("WebSocket client connected");
+  // Handle WebSocket events here
+  socket.on("send_status", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive_status", data);
+  });
+});
+
+const port = process.env.PORT || 5001
+server.listen(port, () => {
+  console.log(`WebSocket server running on port ${port}`);
+});*/
