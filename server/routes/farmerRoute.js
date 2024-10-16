038957fe788720e11cdbba116d20f3c8e62a13fc
nimba005 <nimbaoyugi@gmail.com>
2024-10-16 22:32:45 +0300

    Added farmerroute for CRUD operations

--- server/routes/farmerRoute.js
+++ server/routes/farmerRoute.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const farmerController = require("../controllers/farmerController");
-const { project } = require('../middlewares/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 
+route.get('/', protect, farmerController.getAllFarmers);
+route.post('/', protect, farmerController.createFarmer);
+router.get('/:id', protect, farmerController.getFarmerById);
+router.put('/:id', protect, farmerController.updateFarmer);
+router.delete('/:id', protect, farmerController.deleteFarmer);
+
+module.exports = router;
