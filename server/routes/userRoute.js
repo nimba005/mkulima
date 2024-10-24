56ed38d48df76caf7a2756a0258a13593043ccaf
nimba005 <nimbaoyugi@gmail.com>
2024-10-24 22:06:50 +0300

    Added userRoute components for user related routes

---
+++ server/routes/userRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser, getUserProfile} = require('../controllers/userController');
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+router.route('/').get(getUserprofile);
+
+module.exports = router;
