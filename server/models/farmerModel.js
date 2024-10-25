7636686c055028c57e5b18386f11bccb26e7e708
nimba005 <nimbaoyugi@gmail.com>
2024-10-25 23:47:13 +0300

    Added farmermodels for connecting with MongoDB

---
+++ server/models/farmerModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const farmerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: Number,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+const Farmer = mongoose.model('Farmer', farmerSchema);
+module.exports = mongoose.model('Farmer', farmerSchema);
